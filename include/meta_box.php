cae6a6f6c649100460c022b0be8cf1378c52b586
hijiri <hijiri@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-25 06:46:43 +0000

    ver.1.1.1
    git-svn-id: http://plugins.svn.wordpress.org/advanced-schedule-posts/tags/1.1.0@1098963 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ include/meta_box.php
@@ -0,0 +1,37 @@
+<?php
+
+global $post_id;
+
+$post_type = get_post_type();
+$post_list = $this->get_post_list( $post_type, $post_id );
+
+$hasp_expire_enable = get_post_meta( $post_id, 'hasp_expire_enable', true );
+$hasp_expire_date = get_post_meta( $post_id, 'hasp_expire_date', true ) ? date( 'Y-m-d H:i', strtotime( get_post_meta( $post_id, 'hasp_expire_date', true ) ) ) : null;
+$hasp_overwrite_enable = get_post_meta( $post_id, 'hasp_overwrite_enable', true );
+$hasp_overwrite_post_id = get_post_meta( $post_id, 'hasp_overwrite_post_id', true );
+
+?>
+
+<p><label><input type="checkbox" name="hasp_expire_enable" id="hasp_expire_enable" <?php if( $hasp_expire_enable == 1 ) echo 'checked="checked"'; ?>><?php _e( 'Datetime of expiration', 'hasp' ) ?></label></p>
+
+<div id="hasp_expire_div" style="display: none;">
+
+	<span><input type="text" id="hasp_expire_date" name="hasp_expire_date" size="13" placeholder="<?php _e( 'Y-m-d H:i', 'hasp' ) ?>" value="<?php echo $hasp_expire_date ?>"></span>
+	<span id="hasp_expire_error_1" class="hasp_error_mes" style="display: none"><?php _e( 'Input datetime.', 'hasp' ) ?></span>
+	<span id="hasp_expire_error_2" class="hasp_error_mes" style="display: none"><?php _e( 'Input future datetime.', 'hasp' ) ?></span>
+	
+</div>
+
+<p><label><input type="checkbox" name="hasp_overwrite_enable" id="hasp_overwrite_enable" <?php if( $hasp_overwrite_enable == 1 ) echo 'checked="checked"'; ?>><?php _e( 'Overwrite the another post', 'hasp' ) ?></label></p>
+
+<div id="hasp_overwrite_div" style="display: none;">
+
+	<select name="hasp_overwrite_post_id" id="hasp_overwrite_post_id">
+		<option value="0">— <?php _e( 'Select' ) ?> —</option>
+		<?php foreach( $post_list as $post ) : ?>
+			<option value="<?php echo $post->ID ?>" <?php if( $hasp_overwrite_post_id == $post->ID ) echo 'selected'?>><?php echo $post->post_title ?></option>
+		<?php endforeach; ?>
+	</select>
+	<span id="hasp_overwrite_error" class="hasp_error_mes" style="display: none"><?php _e( 'Select the post.', 'hasp' ) ?></span>
+	<p><?php _e( 'You can set schedule which overwrites the another post.', 'hasp' ) ?></p>
+</div>
